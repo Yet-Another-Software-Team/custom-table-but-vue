226a03b2d8ed80c14edb63970412ef9ccd3bdf22
Sirapob PHASUT <omegaooh@gmail.com>
2025-08-26 14:51:41 +0700

    feat: install Vue Router

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
+import HomeView from './pages/HomeView.vue'
+import AboutView from './pages/AboutView.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/about', name: 'about', component: AboutView },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
