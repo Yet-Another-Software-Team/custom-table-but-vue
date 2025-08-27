7979da190215ce82e2dc648008791997ea2c0f93
Sirapob PHASUT <omegaooh@gmail.com>
2025-08-27 14:25:05 +0700

    feat: Implement Manual Data Input

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <h1>Hello App!</h1>
-  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
-  <nav>
-    <RouterLink to="/" class="text-blue-500 mr-5">Home</RouterLink>
-    <RouterLink to="/about" class="text-blue-500">About</RouterLink>
-  </nav>
   <main>
     <RouterView />
   </main>
--- app/src/pages/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-<div>
-    <h1 class="text-3xl text-green-300">About</h1>
-    <p>This is the about page.</p>
-</div>
-</template>
