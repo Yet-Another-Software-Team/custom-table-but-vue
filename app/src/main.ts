f29c5aa6beb7e574c23869307abe15deb5693dee
Phasin Sae-ngow <bird.biob3@gmail.com>
2025-08-27 15:09:28 +0700

    fix: remove AboutView from import and routes

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,11 +3,10 @@ import './style.css'
 import App from './App.vue'
 import { createWebHistory, createRouter } from 'vue-router'
 import HomeView from './pages/HomeView.vue'
-import AboutView from './pages/AboutView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
-  { path: '/about', name: 'about', component: AboutView },
+  { path: '/about', name: 'about' },
 ]
 
 const router = createRouter({
